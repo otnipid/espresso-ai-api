2c4c19f1014d7cedab09820b8576d59c2e59f72c
otnipd <ndipinto@protonmail.com>
2026-04-03 23:54:09 +0800

    fix: Added bind argument to all user and gronder routes

--- src/routes/grinder.routes.ts
+++ src/routes/grinder.routes.ts
@@ -4,21 +4,21 @@ import GrinderController from '../controllers/grinder.controller';
 const router = Router();
 
 // GET /api/grinders
-router.get('/', GrinderController.all);
+router.get('/', GrinderController.all.bind(GrinderController));
 
 // POST /api/grinders
-router.post('/', GrinderController.save);
+router.post('/', GrinderController.save.bind(GrinderController));
 
 // GET /api/grinders/:id
-router.get('/:id', GrinderController.one);
+router.get('/:id', GrinderController.one.bind(GrinderController));
 
 // PUT /api/grinders/:id
-router.put('/:id', GrinderController.update);
+router.put('/:id', GrinderController.update.bind(GrinderController));
 
 // DELETE /api/grinders/:id
-router.delete('/:id', GrinderController.remove);
+router.delete('/:id', GrinderController.remove.bind(GrinderController));
 
 // GET /api/grinders/by-manufacturer
-router.get('/by-manufacturer', GrinderController.getGrindersByManufacturer);
+router.get('/by-manufacturer', GrinderController.getGrindersByManufacturer.bind(GrinderController));
 
 export default router;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,21 +4,21 @@ import UserController from '../controllers/user.controller';
 const router = Router();
 
 // GET /api/users
-router.get('/', UserController.all);
+router.get('/', UserController.all.bind(UserController));
 
 // POST /api/users
-router.post('/', UserController.save);
+router.post('/', UserController.save.bind(UserController));
 
 // GET /api/users/:id
-router.get('/:id', UserController.one);
+router.get('/:id', UserController.one.bind(UserController));
 
 // PUT /api/users/:id
-router.put('/:id', UserController.update);
+router.put('/:id', UserController.update.bind(UserController));
 
 // DELETE /api/users/:id
-router.delete('/:id', UserController.remove);
+router.delete('/:id', UserController.remove.bind(UserController));
 
 // GET /api/users/by-email
-router.get('/by-email', UserController.getUsersByEmail);
+router.get('/by-email', UserController.getUsersByEmail.bind(UserController));
 
 export default router;
